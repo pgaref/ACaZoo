be462483b991d1b4e2511d09a6a1d4b1ffb10983
pgaref@ubuntu <pgaref@pgaref-OptiPlex-755>
2013-11-28 20:50:15 +0200

    cc

--- src/java/org/apache/zookeeper/server/ZKDatabase.java
+++ src/java/org/apache/zookeeper/server/ZKDatabase.java
@@ -342,7 +342,7 @@ public class ZKDatabase {
 					//Its the first Znode!
 					long tmp = CommitLog.log_count;
 					try {
-						this.dataTree.deleteNode("/cassandra"+String.format("%015d", tmp),tmp);
+						this.dataTree.deleteNode("/cassandra"+String.format("%015d", tmp)+"/",tmp);
 					} catch (NoNodeException e) {
 						LOG.error("pgaref - CaZoo F Cannot delete previous Znode!!!" +tmp + "Hdr ID: "+ hdr.getZxid() + " | "+ e.toString());
 					}
