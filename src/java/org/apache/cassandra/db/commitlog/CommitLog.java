6243f5165627a11319927f4c5f631a4316b802c2
pgaref@ubuntu <pgaref@pgaref-OptiPlex-755>
2013-11-28 22:24:20 +0200

    aaaa

--- src/java/org/apache/cassandra/db/commitlog/CommitLog.java
+++ src/java/org/apache/cassandra/db/commitlog/CommitLog.java
@@ -229,7 +229,7 @@ public class CommitLog implements CommitLogMBean
 					logger.error("pgaref - LEADER: Serializer exception!");
 					e.printStackTrace();
 				}
-	    		org.apache.cassandra.service.CassandraDaemon.ZooServer.insertPersistent("/cassandra"+String.format("%015d", log_count), baos.toByteArray());
+	    		org.apache.cassandra.service.CassandraDaemon.ZooServer.insertPersistent("/cassandra"+String.format("%015d", ++log_count), baos.toByteArray());
 	    		
 	    		/*pgaref Master - Now Delete previous Znode !!*/
 		    	if(log_count > 1L){
