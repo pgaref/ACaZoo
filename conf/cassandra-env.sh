312dec419b5c74154f6987961afea1348d6bda00
pgaref@ubuntu <pgaref@pgaref-OptiPlex-755>
2013-12-01 02:23:56 +0200

    fix environment

--- conf/cassandra-env.sh
+++ conf/cassandra-env.sh
@@ -129,8 +129,8 @@ esac
 # times. If in doubt, and if you do not particularly want to tweak, go with
 # 100 MB per physical CPU core.
 
-#MAX_HEAP_SIZE="2G"
-#HEAP_NEWSIZE="200M"
+MAX_HEAP_SIZE="2G"
+HEAP_NEWSIZE="200M"
 
 if [ "x$MAX_HEAP_SIZE" = "x" ] && [ "x$HEAP_NEWSIZE" = "x" ]; then
     calculate_heap_sizes
